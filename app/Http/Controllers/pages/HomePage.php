7d443d44b85023cb7db9df92eb01e47193b7e179
yusufye <email.yusufye@gmail.com>
2025-03-10 18:32:08 +0700

    dashboard

--- app/Http/Controllers/pages/HomePage.php
+++ app/Http/Controllers/pages/HomePage.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers\pages;
 
-use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class HomePage extends Controller
 {
   public function index()
   {
-    return view('content.pages.pages-home');
+    $order= Order::when(auth()->user()->hasRole('admin'), function ($query) {
+        $query->where('created_by',auth()->user()->id);
+    })
+    ->get();
+
+    $order_checked=Order::where('status', 'APPROVED')->where('approval_step', 3) ->whereHas('orderChecklist') ->count();
+
+    
+    return view('content.pages.pages-home',compact('order','order_checked'));
   }
 }
