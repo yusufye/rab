e596b4a36b29d9beae01b16d4722cc400f4c9c8a
yusufye <email.yusufye@gmail.com>
2025-05-05 17:28:34 +0700

    change favicon

--- resources/views/layouts/commonMaster.blade.php
+++ resources/views/layouts/commonMaster.blade.php
@@ -22,7 +22,7 @@ $contentLayout = (isset($container) ? (($container === 'container-xxl') ? "layou
   <!-- Canonical SEO -->
   <link rel="canonical" href="{{ config('variables.productPage') ? config('variables.productPage') : '' }}">
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/favicon.ico') }}" />
+  <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/favicon/esdm_favicon.ico') }}" />
 
   {{-- remove while creating package --}}
   @if(env('APP_ENV') === 'production')
